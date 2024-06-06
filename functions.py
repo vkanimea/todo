bf4bd816b5387232bb70961972c628df1cf9ae51
Vincent Kanimea <vkanimea@gmail.com>
2024-06-06 13:11:30 +1200

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,16 @@
+# Separate functions in own file and main logic is in the main.py
+FILEPATH = 'todos.txt'
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the List of
+    to-do items
+    """
+    with open(filepath, 'r') as file:
+        todos = file.readlines()
+    return todos
+
+def write_todos(todos_arg,filepath=FILEPATH):
+    """ Write the to-do items list in the text file
+    """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
